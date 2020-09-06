dc38307ca70cdaa34bcd36e7d9ae340f7b6953c6
Enrico Berti <hello@enricoberti.com>
2020-09-06 22:34:38 +0200

    Display repo status and parents info

--- src/main.js
+++ src/main.js
@@ -18,10 +18,6 @@ import "vue-prism-editor/dist/VuePrismEditor.css"; // import the styles
 
 Vue.component("prism-editor", VuePrismEditor);
 
-import JsonViewer from "vue-json-viewer";
-
-Vue.use(JsonViewer);
-
 Vue.use(VueClipboard);
 
 library.add({
