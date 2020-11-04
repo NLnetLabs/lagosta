13a7918da2a786320db6e000bb5f3415ff0a36fb
Tim Bruijnzeels <tim@nlnetlabs.nl>
2020-11-04 10:40:51 +0100

    Add status information for AS0 REDUNDANT.

--- src/components/StateVisualizer.vue
+++ src/components/StateVisualizer.vue
@@ -24,8 +24,17 @@
       v-if="scope.row.state === 'roa_as0'"
       disable-transitions
       effect="plain"
-      :title="$t('announcements.state.as0_help')"
-      >{{ $t("announcements.state.as0") }}</el-tag
+      :title="$t('announcements.state.roa_as0_help')"
+      >{{ $t("announcements.state.roa_as0") }}</el-tag
+    >
+    <el-tag
+        type="warning"
+        size="medium"
+        v-if="scope.row.state === 'roa_as0_redundant'"
+        disable-transitions
+        effect="plain"
+        :title="$t('announcements.state.roa_as0_redundant_help')"
+    >{{ $t("announcements.state.roa_as0_redundant") }}</el-tag
     >
     <el-tag
       type="success"
